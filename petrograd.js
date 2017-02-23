4463fcd5b202cb13d5807726548d8ab33a1faf3a
Marie Støving Rørstrøm <Marie@Maries-MacBook-Pro.local>
2017-02-23 15:26:18 +0100

    load JSON produktliste and create template clone for each

--- petrograd.js
+++ petrograd.js
@@ -2,10 +2,20 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("Siden vises");
+
+    // LÆS PRODUKTLISTE
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
     visProdukt();
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt);
     // KLON PRODUKT_TEMPLATE
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
