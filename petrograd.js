ac175f803dcad224005ab3f863ab4baf18770ff9
Marie Støving Rørstrøm <Marie@Maries-MacBook-Pro.local>
2017-02-23 16:02:34 +0100

    add / remove rabat when no rabat

--- petrograd.js
+++ petrograd.js
@@ -26,6 +26,22 @@ function visProdukt(produkt) {
 
     klon.querySelector(".data_billede").src = "/imgs/small/" + produkt.billede + "-sm.jpg";
 
+    if (produkt.udsolgt == false) {
+        // PRODUKTET ER IKKE UDSOLGT
+        // UDSOLGTTEKST SKAL FJERNES
+        var udsolgttekst = klon.querySelector(".udsolgttekst");
+        udsolgttekst.parentNode.removeChild(udsolgttekst);
+    } else {
+        klon.querySelector(".pris").classList.add("udsolgt");
+    }
+
+    // DER ER IKKE RABAT. RABATPRISEN SKAL FJERNES
+    if (produkt.udsolgt == true || produkt.rabatsats == 0) {
+        var rabatpris = klon.querySelector(".rabatpris");
+        rabatpris.parentNode.removeChild(rabatpris);
+    } else {
+        klon.querySelector(".pris").classList.add("rabat");
+    }
     // APPEND KLON TIL .PRODUKTLISTE
     document.querySelector(".produktliste").appendChild(klon);
 }
