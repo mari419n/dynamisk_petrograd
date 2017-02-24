717b9c485d0257b9ce69740fb3d350deab1cd57e
Marie Støving Rørstrøm <Marie@desktop-5m7kc8e.kea.local>
2017-02-24 10:48:35 +0100

    html-orden, modal-knap

--- petrograd.js
+++ petrograd.js
@@ -21,11 +21,18 @@ function visProdukt(produkt) {
     klon.querySelector(".data_navn").innerHTML = produkt.navn;
     klon.querySelector(".data_pris").innerHTML = produkt.pris;
 
+    // VIS RABAT
     var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
     klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
+    // VIS BILLEDE
     klon.querySelector(".data_billede").src = "/imgs/small/" + produkt.billede + "-sm.jpg";
 
+    // MODALKNAP
+    klon.querySelector("button").dataset.id = produkt.id;
+    klon.querySelector("button").addEventListener("click", knapKlikketPå)
+
+    // UDSOLGT
     if (produkt.udsolgt == false) {
         // PRODUKTET ER IKKE UDSOLGT
         // UDSOLGTTEKST SKAL FJERNES
@@ -42,6 +49,33 @@ function visProdukt(produkt) {
     } else {
         klon.querySelector(".pris").classList.add("rabat");
     }
+
     // APPEND KLON TIL .PRODUKTLISTE
-    document.querySelector(".produktliste").appendChild(klon);
+    //document.querySelector(".produktliste").appendChild(klon);
+    document.querySelector(".forretter").appendChild(klon);
+    console.log("." + produkt.kategori)
+        // document.querySelector("." + produkt.kategori).appendChild(klon);
+
+    /*if (produkt.kategori == "forretter") {
+        document.querySelector(".forretter").appendChild(klon);
+    } else if ()*/
+
+}
+
+function knapKlikketPå(oplysningerOmEventet) {
+    document.querySelector("#myModalLabel").textContent = "Loader...";
+    document.querySelector("#myModal .modal-body p").textContent = "...";
+
+    var produktId = oplysningerOmEventet.target.dataset.id;
+
+    // SEND FORESPØRGSEL TIL http://petlatkea.dk/2017/dui/api/product?callback=?&id=21 MED DET RIGTIGE ID
+    $.getJSON("http://petlatkea.dk/2017/dui/api/product?callback=?&id=" + produktId, visModalIndhold);
+
+}
+
+function visModalIndhold(mereInfo) {
+    console.log(mereInfo);
+
+    document.querySelector("#myModalLabel").textContent = mereInfo.navn;
+    document.querySelector("#myModal .modal-body p").textContent = mereInfo.langbeskrivelse;
 }
